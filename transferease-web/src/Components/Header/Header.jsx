33308517ddbe78062f4ae2c4f4a7ab65bae20532
Kavinda <kavindaljayarathna725@gmail.com>
2024-04-24 23:56:05 +0530

    edit header component

--- transferease-web/src/Components/Header/Header.jsx
+++ transferease-web/src/Components/Header/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='headerContainor'>
+      <div className="logo">
+        <h2>TransferEase</h2>
+      </div>
+      <div className="menuContainor">
+        <ul className='menu'>
+          <li><a href="">Home</a></li>
+          <li><a href="">Downloads</a></li>
+          <li><a href="">Transfer</a></li>
+          <li><a href="">Contact</a></li>
+          <li><a href="">Home</a></li>
+          <li><button>Sign Up</button></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
