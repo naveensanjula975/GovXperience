bb9e2cefefd1163a98273efd2302d666ca71df45
Kavinda <kavindaljayarathna725@gmail.com>
2024-05-13 10:56:31 +0530

    remove fonts

--- transferease-web/src/Components/Header/Header.jsx
+++ transferease-web/src/Components/Header/Header.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import './Header.css';
-import "@fontsource/poppins";
+import React from "react";
+import "./Header.css";
+// import "@fontsource/poppins";
 
 const Header = () => {
   return (
-    <div className='headerContainor'>
+    <div className="headerContainor">
       <div className="logo">
         <h2>TransferEase</h2>
       </div>
       <div className="menuContainor">
-        <ul className='menu'>
-          <li><a href="">Home</a></li>
-          <li><a href="">Downloads</a></li>
-          <li><a href="">Transfer</a></li>
-          <li><a href="">Contact</a></li>
-          <li><a href="">Login</a></li>
-          <li><button id="van">Sign Up</button></li>
+        <ul className="menu">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Downloads</a>
+          </li>
+          <li>
+            <a href="">Transfer</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+          <li>
+            <a href="">Login</a>
+          </li>
+          <li>
+            <button id="van">Sign Up</button>
+          </li>
         </ul>
       </div>
-      </div>
-  
-  )
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- transferease-web/src/Components/Hero/Hero.jsx
+++ transferease-web/src/Components/Hero/Hero.jsx
@@ -1,34 +1,45 @@
-import React from 'react';
-import './Hero.css';
-import "@fontsource/cabin";
-import "@fontsource/poppins";
-import abc from './Toyota.png';
-
-
+import React from "react";
+import "./Hero.css";
+// import "@fontsource/cabin";
+// import "@fontsource/poppins";
+import abc from "./Toyota.png";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="image">
-        <div className="pro"><img src={abc} className="img"/></div>
+        <div className="pro">
+          <img src={abc} className="img" />
+        </div>
       </div>
       <div className="title">
-        <h1 id="a">Vehicle Ownership<br/>Transfer<span className='ease'> Easy.</span></h1>
-        <br/>
-        <h6 id="b">We aim to streamline the vehicle owner transfer process that ensure a<br/>
-         smooth and secure transfer by eliminating the need for owners and<br/>
-         buyers to do manual long processes.</h6>
-         <br/>
-         </div>
-         <div id="d">
-          <div>
-         <button className="buttons">Transfer Now</button>
+        <h1 id="a">
+          Vehicle Ownership
+          <br />
+          Transfer<span className="ease"> Easy.</span>
+        </h1>
+        <br />
+        <h6 id="b">
+          We aim to streamline the vehicle owner transfer process that ensure a
+          <br />
+          smooth and secure transfer by eliminating the need for owners and
+          <br />
+          buyers to do manual long processes.
+        </h6>
+        <br />
       </div>
-      <h6><a id="c" href="url">More Details<i class="arrow right"></i></a></h6>
+      <div id="d">
+        <div>
+          <button className="buttons">Transfer Now</button>
+        </div>
+        <h6>
+          <a id="c" href="url">
+            More Details<i class="arrow right"></i>
+          </a>
+        </h6>
       </div>
-      </div>
-    
-  )
-}
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
